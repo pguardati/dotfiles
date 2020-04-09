08ea0dff9a592eb34945f0c973ae0f9d82b73871
PietroGuardati <pietroguardati@gmail.com>
2020-04-09 21:41:08 +0200

    tmp - add tmux

--- symlink.sh
+++ symlink.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
+#editor
 ln -nfs /Users/pietroguardati/dev/dotfiles/.vimrc /Users/pietroguardati/.vimrc
 ln -nfs /Users/pietroguardati/dev/dotfiles/.ideavimrc /Users/pietroguardati/.ideavimrc
-
+#terminal
 ln -nfs /Users/pietroguardati/dev/dotfiles/.bashrc /Users/pietroguardati/.bashrc
-
+ln -nfs /Users/pietroguardati/dev/dotfiles/.tmux.config /Users/pietroguardati/.tmux.config
