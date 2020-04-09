85370a56f7db98c60d7f63c2c963516a0abd48ac
PietroGuardati <pietroguardati@gmail.com>
2020-04-09 21:19:54 +0200

    tmp - add bashrc

--- symlink.sh
+++ symlink.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-ln -nfs /Users/pietroguardati/dev/.vimrc /Users/pietroguardati/.vimrc
-ln -nfs /Users/pietroguardati/dev/.ideavimrc /Users/pietroguardati/.ideavimrc
+ln -nfs /Users/pietroguardati/dev/dotfiles/.vimrc /Users/pietroguardati/.vimrc
+ln -nfs /Users/pietroguardati/dev/dotfiles/.ideavimrc /Users/pietroguardati/.ideavimrc
 
+ln -nfs /Users/pietroguardati/dev/dotfiles/.bashrc /Users/pietroguardati/.bashrc
 
