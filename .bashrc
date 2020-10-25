701c439766425b99db4c63468357fb1e5f2449bf
PietroGuardati <pietroguardati@gmail.com>
2020-10-25 14:57:05 +0100

    feat: add common profile for .bashrc and .zshrc

--- .bashrc
+++ .bashrc
@@ -1,13 +1,4 @@
+source ~/.profile
+
 # activate vim in bash
 set -o vi
-# export python projects
-PYTHONPATH=~/first_project:$PYTHONPATH
-PYTHONPATH=~/second_project:$PYTHONPATH
-export PYTHONPATH
-
-
-
-
-
-
-
--- symlink.sh
+++ symlink.sh
@@ -3,6 +3,10 @@
 #editor
 ln -nfs /Users/pietroguardati/dev/dotfiles/.vimrc /Users/pietroguardati/.vimrc
 ln -nfs /Users/pietroguardati/dev/dotfiles/.ideavimrc /Users/pietroguardati/.ideavimrc
+
 #terminal
+ln -nfs /Users/pietroguardati/dev/dotfiles/.profile /Users/pietroguardati/.profile
 ln -nfs /Users/pietroguardati/dev/dotfiles/.bashrc /Users/pietroguardati/.bashrc
+ln -nfs /Users/pietroguardati/dev/dotfiles/.zshrc /Users/pietroguardati/.zshrc
 ln -nfs /Users/pietroguardati/dev/dotfiles/.tmux.config /Users/pietroguardati/.tmux.config
+
