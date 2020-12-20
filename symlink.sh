f1dd92024b37bf02fb0a66da0741da307d31dd28
PietroGuardati <pietroguardati@gmail.com>
2020-12-20 20:16:58 +0100

    refactor: move config files do centralised project directory

--- symlink.sh
+++ symlink.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #editor
-ln -nfs /Users/pietroguardati/dev/dotfiles/.vimrc /Users/pietroguardati/.vimrc
-ln -nfs /Users/pietroguardati/dev/dotfiles/.ideavimrc /Users/pietroguardati/.ideavimrc
+ln -nfs /Users/pietroguardati/PycharmProjects/dotfiles/.vimrc /Users/pietroguardati/.vimrc
+ln -nfs /Users/pietroguardati/PycharmProjects/dotfiles/.ideavimrc /Users/pietroguardati/.ideavimrc
 
 #terminal
-ln -nfs /Users/pietroguardati/dev/dotfiles/.profile /Users/pietroguardati/.profile
-ln -nfs /Users/pietroguardati/dev/dotfiles/.bashrc /Users/pietroguardati/.bashrc
-ln -nfs /Users/pietroguardati/dev/dotfiles/.zshrc /Users/pietroguardati/.zshrc
-ln -nfs /Users/pietroguardati/dev/dotfiles/.tmux.config /Users/pietroguardati/.tmux.config
+ln -nfs /Users/pietroguardati/PycharmProjects/dotfiles/.profile /Users/pietroguardati/.profile
+ln -nfs /Users/pietroguardati/PycharmProjects/dotfiles/.bashrc /Users/pietroguardati/.bashrc
+ln -nfs /Users/pietroguardati/PycharmProjects/dotfiles/.zshrc /Users/pietroguardati/.zshrc
+ln -nfs /Users/pietroguardati/PycharmProjects/dotfiles/.tmux.config /Users/pietroguardati/.tmux.config
 
