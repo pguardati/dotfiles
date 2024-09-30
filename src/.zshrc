b252b5221d6ef1335aae792a8e765b7095ee82ab
pguardati <pietroguardati@gmail.com>
2024-10-01 01:04:02 +0200

    chore: add vscode and cursor keybindings

--- src/.zshrc
+++ src/.zshrc
@@ -104,3 +104,11 @@ source $ZSH/oh-my-zsh.sh
 
 # activate vim in bash
 set -o vi
+
+# java
+export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
--- src/install_dotfiles.sh
+++ src/install_dotfiles.sh
@@ -3,6 +3,8 @@
 
 DEFAULT_SOURCE_PATH="$HOME/PycharmProjects/dotfiles/src"
 DEFAULT_DESTINATION_PATH="$HOME"
+CURSOR_KEYBINDINGS_DIR="$HOME/Library/Application Support/Cursor/User"
+VSCODE_KEYBINDINGS_DIR="$HOME/Library/Application Support/Code/User"
 
 prompt_user() {
   read -p "Do you want to use the default paths?
@@ -20,16 +22,22 @@ Destination: $DEFAULT_DESTINATION_PATH
 }
 
 create_links() {
+
   # Create symbolic links for editor configurations
   ln -nfs $SOURCE_PATH/.vimrc $DESTINATION_PATH/.vimrc
   ln -nfs $SOURCE_PATH/.ideavimrc $DESTINATION_PATH/.ideavimrc
-
   # Create symbolic links for terminal configurations
   ln -nfs $SOURCE_PATH/.profile $DESTINATION_PATH/.profile
   ln -nfs $SOURCE_PATH/.bashrc $DESTINATION_PATH/.bashrc
   ln -nfs $SOURCE_PATH/.zshrc $DESTINATION_PATH/.zshrc
-
   echo "Dotfiles linked from $SOURCE_PATH to $DESTINATION_PATH"
+
+  # Link VSCode keybindings to Cursor keybindings
+  ln -nfs "$DEFAULT_SOURCE_PATH/../ide_vscode/keybindings.json" "$CURSOR_KEYBINDINGS_DIR/keybindings.json"
+  echo "VSCode keybindings linked to Cursor at $CURSOR_KEYBINDINGS_DIR/keybindings.json"
+
+  ln -nfs "$DEFAULT_SOURCE_PATH/../ide_vscode/keybindings.json" "$VSCODE_KEYBINDINGS_DIR/keybindings.json"
+  echo "VSCode keybindings linked to VSCode at $VSCODE_KEYBINDINGS_DIR/keybindings.json"
 }
 
 main() {
